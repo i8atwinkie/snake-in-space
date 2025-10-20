6b79686214758a8581615aa0e2a5813e8eff527b
i8atwinkie <i8atwinkie@gmail.com>
2025-10-19 22:23:15 -0600

    snake segments will now follow a path instead of inverse kinematics

--- scenes/player/segment_controller.gd
+++ scenes/player/segment_controller.gd
@@ -1,19 +1,31 @@
 extends Node2D
 
+@onready var parent = get_parent() 
+
 @export var segment_scene: PackedScene
+
+@export var segment_interval: float = 5
+
+var snake_path_points: Array[Vector2] = []
 
 var _children: Array[Node]
 
 func _ready() -> void:
 	_children = get_children()
 
+func _physics_process(delta: float) -> void:
+	snake_path_points.push_front(parent.global_position)
+	if snake_path_points.size() > 100:
+		snake_path_points.pop_back()
+	
+	for i in range(_children.size()):
+		if snake_path_points.size() > (i + 1) * 5:
+			_children[i].global_position = snake_path_points[(i + 1) * 5]
+		elif snake_path_points.size() > 0:
+			_children[i].global_position = snake_path_points.back()
+
 func add_segment() -> void:
 	var new_segment = segment_scene.instantiate()
-	var inverse_kinematics_component = new_segment.get_node("./InverseKinematics") as InverseKinematics
-	if _children.size() > 0:
-		inverse_kinematics_component.following = _children.back()
-	else:
-		inverse_kinematics_component.following = self
 	add_child(new_segment)
 	_children.append(new_segment)
 
